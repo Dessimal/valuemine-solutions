bc5ca60e3ea586afa96c462e6a1093177bbd857a
Dessimal <you@example.com>
2025-05-26 11:22:09 +0100

    removed unused files

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// export function CalculateMonthlyPayment(
+//   packageCost: number,
+//   location: string,
+//   workmanship: number,
+//   buildingType: string,
+//   interestRate: number,
+//   years: number
+// ): number {
+//   const monthlyInterestRate = 5.5 / 100 ; // Monthly interest rate (5.5% monthly rate)
+//   const numberOfPayments = 12;
+
+//   const monthlyPayment =
+//     (principal * monthlyInterestRate) /
+//     (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
+
+//   return monthlyPayment;
+// }
