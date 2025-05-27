0dcee8a6502c4f969157c5c786a55c55fc1cb922
Dessimal <you@example.com>
2025-05-27 05:30:58 +0100

    tried resolving issue with interest calculator file path

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const page = () => {
   return (
-    <main className="grid items-center justify-center pt-16 sm:pt-20 px-10 sm:px-14 min-h-screen bg-gradient-to-bl from-yellow-600 to-orange-400">
-      <div className="bg-white shadow-lg rounded-lg p-4 sm:p-8 mx-auto grid items-center justify-center">
+    <main className="justify-center pt-16 sm:pt-20 p-4 sm:px-10 sm:px-14 min-h-screen bg-gradient-to-bl from-yellow-600 to-orange-400">
+      <div className="w-full bg-white shadow-lg rounded-lg p-4 sm:p-8 mx-auto grid items-center justify-center">
         <SizeCalculator />
       </div>
     </main>
