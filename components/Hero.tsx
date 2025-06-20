52e6987ee68631ecf3797a6b275c8c3fb92e0451
Dessimal <you@example.com>
2025-06-20 12:55:21 +0100

    added some framer animation to the hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,15 +5,21 @@ import Image from "next/image";
 import { MainHeroImage } from "@/app/constants";
 import { HeroImage } from "../app/constants";
 import { ArrowRight } from "lucide-react";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const router = useRouter();
 
   return (
     <section className="w-screen min-h-screen ">
-      <div className="container md:py-16 py-32 px-4 flex flex-col md:flex-row md:items-center justify-center gap-12 border-2 border-red h-full mx-auto hero-container">
+      <div className="container md:py-16 py-32 px-4 flex flex-col md:flex-row md:items-center justify-center gap-12 h-full mx-auto hero-container">
         <div className="flex flex-1/2 flex-col gap-8 border-blue-800">
-          <div className="flex flex-1/2 flex-col gap-1">
+          <motion.div
+            initial={{ opacity: 0, x: -20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.7 }}
+            className="flex flex-1/2 flex-col gap-1">
             <h1 className="text-5xl md:text-9xl font-extrabold tracking-tight">
               Say Goodbye to Nepa Wahala
             </h1>
@@ -22,7 +28,7 @@ const Hero = () => {
               Discover sustainable, reliable power solutions for your home and
               business. Join the solar revolution today!
             </p>
-          </div>
+          </motion.div>
           <div className="flex-1/2">
             <Button
               className="gradient-bg"
@@ -32,11 +38,17 @@ const Hero = () => {
             </Button>
           </div>
         </div>
-        <div className="relative flex-1/2 h-full min-h-[560px] min-w-[400px] w-full p-7 border-2 border-green-500 overflow-hidden rounded-md">
-          <div className="relative h-full w-full rounded-md shadow-lg ">
+        <motion.div
+          initial={{ opacity: 0, x: 20 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          className="flex-1/2 p-6 border-2 rounded-md">
+          <div className="relative h-[560px] w-[560px] md:w-[964px] ring ring-gray-800 rounded-md overflow-hidden shadow-lg ">
             <Image
               src={MainHeroImage}
-              className="object-cover"
+              className="object-cover       
+              "
               fill
               alt="happy family using solar"
             />
@@ -47,7 +59,7 @@ const Hero = () => {
             width={200}
             className="rounded-full ring ring-yellow-500 shadow-md absolute -top-6 -left-6"
           /> */}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
