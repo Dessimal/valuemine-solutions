f8616aeccbaa07307449a476c6f61d0ee360da6a
Dessimal <you@example.com>
2025-07-17 15:42:21 +0100

    resolving issue with authlayout and routing. Added a separate layout for pages with navbar

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,45 +1,17 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Card } from "@/components/ui/card";
 import { Toaster } from "sonner";
-import { Inter, Mulish, Urbanist } from "next/font/google";
 
-const mulish = Mulish({
-  subsets: ["latin"],
-  variable: "--font-mulish",
-});
-
-const urbanist = Urbanist({
-  subsets: ["latin"],
-  variable: "--font-urbanist",
-});
-
-const layout = ({ children }: { children: React.ReactNode }) => {
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta
-          name="facebook-domain-verification"
-          content="2ngp53js7du0iszol3bufkogxiznlo"
-        />
-      </head>
-      <body className={` ${urbanist.variable} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange>
-          <main>
-            <div className="bg-muted min-h-screen w-full">
-              <div className=" container mx-auto min-h-screen w-full md:max-w-3xl mx-auto flex items-center justify-center p-2 ">
-                {children}
-              </div>
-            </div>
-          </main>
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
+    <main>
+      <div className="bg-muted min-h-screen w-full">
+        <div className=" container mx-auto min-h-screen w-full md:max-w-3xl mx-auto flex items-center justify-center p-2 ">
+          {children}
+        </div>
+      </div>
+    </main>
   );
 };
 
-export default layout;
+export default AuthLayout;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,75 +1,17 @@
-import type { Metadata } from "next";
-import { Inter, Mulish, Urbanist } from "next/font/google";
 import "../globals.css";
 import { Footer } from "@/components/Footer";
-import { Toaster } from "@/components/ui/sonner";
 import { Navbar } from "@/components/Navbar";
-import { ThemeProvider } from "@/components/theme-provider";
 
-const mulish = Mulish({
-  subsets: ["latin"],
-  variable: "--font-mulish",
-});
-
-const urbanist = Urbanist({
-  subsets: ["latin"],
-  variable: "--font-urbanist",
-});
-
-export const metadata: Metadata = {
-  title: "Valuemine Solutions",
-  description: "Sustainable Solutions, Exceptional Value",
-};
-
-export default async function RootLayout({
+export default async function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const session = await auth.api.getSession({
-  //   headers: await headers(), // you need to pass the headers object.
-  // });
-
-  // if (!session) {
-  //   return (
-  //     <html lang="en">
-  //       <head>
-  //         <meta
-  //           name="facebook-domain-verification"
-  //           content="2ngp53js7du0iszol3bufkogxiznlo"
-  //         />
-  //       </head>
-  //       <body className={` ${urbanist.variable} antialiased dark`}>
-  //         <Navbar />
-  //         <main>{children}</main>
-  //         <Footer />
-  //         <Toaster />
-  //       </body>
-  //     </html>
-  //   );
-  // }
-
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta
-          name="facebook-domain-verification"
-          content="2ngp53js7du0iszol3bufkogxiznlo"
-        />
-      </head>
-      <body className={` ${urbanist.variable} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange>
-          <Navbar />
-          <main>{children}</main>
-
-          <Toaster />
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
   );
 }
