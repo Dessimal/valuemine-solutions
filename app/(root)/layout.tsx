7af49507e1a0b54333fd0c76ef19036a1b3602b1
Dessimal <65266493+Dessimal@users.noreply.github.com>
2026-07-28 13:38:06 +0100

    Integrate Poppins font into MainLayout
    Added Poppins font to the layout and updated the main layout div to use it.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import "../globals.css";
-import { Inter, Mulish, Urbanist } from "next/font/google";
+import { Poppins, Inter, Mulish, Urbanist } from "next/font/google";
 
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
@@ -14,13 +14,20 @@ const urbanist = Urbanist({
   variable: "--font-urbanist",
 });
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+
+
 export default async function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className={` ${urbanist.variable} antialiased`}>
+    <div className={` ${poppins.variable} antialiased`}>
       <Navbar />
       <div className="max-w-5xl mx-auto">{children}</div>
     </div>
